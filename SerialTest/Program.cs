44402674a3f3e4fdff9436e9f18a696820026b2e
Chad Walker <chad.walker@versantsc.com>
2016-02-29 15:31:58 -0600

    Visual Studio Test

---
+++ SerialTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerialTest
+{
+    [Serializable]
+    class Person
+    {
+        public int age { get; set; }
+        public string name {get; set;}
+
+        public void Initialize()
+        {
+            age = 1;
+            name = "John";
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+  
+            /*
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("c:\\MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, person);
+            stream.Close();
+            */
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("C:\\MyFile.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+            Person obj = (Person)formatter.Deserialize(stream);
+            stream.Close();
+
+            // Here's the proof.
+            Console.WriteLine("n1: {0}", obj.age);
+            Console.WriteLine("n2: {0}", obj.name);
+            Console.ReadLine();
+            
+
+
+
+
+        }
+    }
+}
---
+++ VisualStudios/XSLT_Transformation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace XSLT_Transformation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                XslCompiledTransform transform = new XslCompiledTransform();
+                transform.Load(@"C:\XML_Test\wordcount.xsl");
+                transform.Transform(@"C:\XML_Test\hello.xml", @"C:\XML_Test\output.xml");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
